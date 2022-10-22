eaac7a64c56c1daac998e3ad260fd849e5cf0802
mikofry745 <mikolaj.frycowski@gmail.com>
2022-10-22 19:48:14 +0200

    Add is organization information to business entity

--- Invoiceify.Entities/Entities/Business.cs
+++ Invoiceify.Entities/Entities/Business.cs
@@ -1,11 +1,16 @@
-﻿namespace Invoiceify.Entities.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoiceify.Entities.Entities;
 
 public class Business
 {
     public int Id { get; set; }
+    
+    [Required]
+    public bool IsOrganization { get; set; }
+    
     public string? DefaultInvoiceNote { get; set; }
     public virtual BusinessProfile? Profile { get; set; }
-    
     public ICollection<Product> Products { get; set; }
     public ICollection<Customer> Customers { get; set; }
     public ICollection<Invoice> Invoices { get; set; }
