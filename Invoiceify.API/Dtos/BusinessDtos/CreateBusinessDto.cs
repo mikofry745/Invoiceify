b52c39502940594b96cd7e737e56852951c8e563
mikofry745 <mikolaj.frycowski@gmail.com>
2022-10-30 01:39:54 +0200

    Add required attribute to createBusinessDto

--- Invoiceify.API/Dtos/BusinessDtos/CreateBusinessDto.cs
+++ Invoiceify.API/Dtos/BusinessDtos/CreateBusinessDto.cs
@@ -1,13 +1,22 @@
-﻿namespace Invoiceify.API.Dtos.InvoiceDtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoiceify.API.Dtos.InvoiceDtos;
 
 public class CreateBusinessDto
 {
+    [Required]
     public bool IsOrganization { get; set; }
     public string? DefaultInvoiceNote { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Street { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
     public string PostalCode { get; set; }
+    [Required]
     public string TaxIdentificationNumber { get; set; }
+    [Required]
     public string EmailAddress { get; set; }
 }
