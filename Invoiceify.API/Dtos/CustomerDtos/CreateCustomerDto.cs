8af20345f00d32fe400eeb7eb4100925682250ef
mikofry745 <mikolaj.frycowski@gmail.com>
2022-10-30 01:40:47 +0200

    Add create and update customer dtos

---
+++ Invoiceify.API/Dtos/CustomerDtos/CreateCustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoiceify.API.Dtos.CustomerDtos;
+
+public class CreateCustomerDto
+{
+    [Required]
+    public string Street { get; set; }
+    [Required]
+    public string City { get; set; }
+    [Required]
+    public string PostalCode { get; set; }
+    [Required]
+    public string TaxIdentificationNumber { get; set; }
+    [Required]
+    public string EmailAddress { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public bool IsOrganization { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Country { get; set; }
+    
+    public int BusinessId { get; set; }
+}
---
+++ Invoiceify.API/Dtos/CustomerDtos/UpdateCustomerDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Invoiceify.API.Dtos.CustomerDtos;
+
+public class UpdateCustomerDto
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string TaxIdentificationNumber { get; set; }
+    public string EmailAddress { get; set; }
+    public string Name { get; set; }
+    public bool IsOrganization { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Country { get; set; }
+}
