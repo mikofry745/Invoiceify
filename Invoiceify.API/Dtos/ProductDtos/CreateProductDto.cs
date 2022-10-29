9e18bfdd041a8f3ffd7e16c12b686c537128e599
mikofry745 <mikolaj.frycowski@gmail.com>
2022-10-29 22:29:55 +0200

    Add create and update product dtos

---
+++ Invoiceify.API/Dtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Invoiceify.Entities.Enums;
+
+namespace Invoiceify.API.Dtos.ProductDtos;
+
+public class CreateProductDto
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public decimal UnitPrice { get; set; }
+    [Required]
+    public UnitType UnitType { get; set; }
+    public string? Description { get; set; }
+    public int? TaxPercentage { get; set; }
+    
+    public int BusinessId { get; set; }
+}
---
+++ Invoiceify.API/Dtos/ProductDtos/UpdateProductDto.cs
@@ -0,0 +1,12 @@
+﻿using Invoiceify.Entities.Enums;
+
+namespace Invoiceify.API.Dtos.ProductDtos;
+
+public class UpdateProductDto
+{
+    public string Name { get; set; }
+    public decimal UnitPrice { get; set; }
+    public UnitType UnitType { get; set; }
+    public string? Description { get; set; }
+    public int? TaxPercentage { get; set; }
+}
