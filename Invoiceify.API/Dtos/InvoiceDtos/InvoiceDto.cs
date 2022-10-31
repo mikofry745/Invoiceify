6ee4989d63bb1a6527fb696c3324e1365c09197b
mikofry745 <mikolaj.frycowski@gmail.com>
2022-10-31 17:41:41 +0100

    Add invoice and invoice product dtos

---
+++ Invoiceify.API/Dtos/InvoiceDtos/InvoiceDto.cs
@@ -0,0 +1,28 @@
+﻿using Invoiceify.API.Dtos.CustomerDtos;
+using Invoiceify.API.Dtos.ProductDtos;
+using Invoiceify.Entities.Enums;
+
+namespace Invoiceify.API.Dtos.InvoiceDtos;
+
+public class InvoiceDto
+{
+    public int Id { get; set; }
+    public string Number { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime IssueDate { get; set; }
+    public InvoiceType Type { get; set; }
+    public CurrencyType Currency { get; set; }
+    public string Note { get; set; }
+    public double SubTotal { get; set; }
+    public double TotalShiping { get; set; }
+    public double TotalPrice { get; set; }
+    public double? TotalTax { get; set; }
+    public DateTime? PaymentDate { get; set; }
+    
+    public int IssuerId { get; set; }
+    public CustomerDto Issuer { get; set; }
+    
+    public int RecipientId { get; set; }
+    public CustomerDto Recipient { get; set; }
+    public List<InvoiceProductDto> Products { get; set; }
+}
---
+++ Invoiceify.API/Dtos/InvoiceDtos/InvoiceProductDto.cs
@@ -0,0 +1,14 @@
+﻿using Invoiceify.Entities.Enums;
+
+namespace Invoiceify.API.Dtos.InvoiceDtos;
+
+public class InvoiceProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal UnitPrice { get; set; }
+    public UnitType UnitType { get; set; }
+    public double Quantity { get; set; }
+    public string? Description { get; set; }
+    public int? TaxPercentage { get; set; }
+}
